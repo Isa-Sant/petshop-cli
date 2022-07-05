809a2ef7f7c733a51fb9641f851797c3e2e53469
Isa-Sant <isabela.phn@hotmail.com>
2022-07-05 14:36:38 +0100

    auteracao no arquivo funcoes.

--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,35 @@
-const cachorros = require('./database/cachorros.json');
+const cachorros = require('./database/cachorros.json')
+const fs = require('fs');
+const path = require('path');
+const { isModuleNamespaceObject } = require('util/types');
 
-function salvar(){}
 
-function buscar(id){}
+function salvar () {
 
-module.exports = {
+    let arquivo = path.resolve('/../database/cachorros.json');
+    let json = JSON.stringify(cachorros, null, 4);
+    
+    // console.log('arquivo: ' + arquivo);
+    // console.log('json: ' + json);
+    
+    fs.writeFileSync(arquivo, json);
+//console.log("arquivo: " + arquivo + " json: " + json);
+
+};
+
+
+// salvar();
+
+function buscar(id){
+    let cachorroBuscado = id;
+    let resultado = cachorros.find(function(cachorroBuscdo){
+        return cachorros.cachorroBuscado == cachorros.id;
+    });
+
+    console.log(resultado);
     
 }
+buscar(3);
+//console.table(resultado);
+
+
